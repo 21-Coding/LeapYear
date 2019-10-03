fc89763f971d4da7ee02a493c54ae12d09c45dc2
DominiqueYoumans <ddyoumans@icloud.com>
2019-10-02 18:10:11 -0700

    add new span in html with cooresponding jquery calls in UI

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,3 @@
-$(document).ready(function() {
-  $("form#leap-year").submit(function(event) {
-    event.preventDefault();
-    var year = parseInt($("input#year").val());
-    var result = leapYear(year);
-    $("#result").text(result);
-
-
-  });
-});
-
 var leapYear = function(year) {
   if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
     return true;
@@ -16,3 +5,21 @@ var leapYear = function(year) {
     return false;
   }
 };
+
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+
+    $(".year").text(year);
+
+    if (!result) {                 // same as writing if (result === false)
+      $(".not").text("not");
+    } else {
+      $(".not").text("");
+    }
+
+    $("#result").show();
+  });
+});
